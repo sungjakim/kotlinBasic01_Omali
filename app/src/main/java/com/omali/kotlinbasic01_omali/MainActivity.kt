049fe2f1b90cd8365d2c9e3de81f3660905c5d20
Omali <misskimlilac68@gmail.com>
2023-04-23 13:31:24 +0900

    로그 찍어 보기

--- app/src/main/java/com/omali/kotlinbasic01_omali/MainActivity.kt
+++ app/src/main/java/com/omali/kotlinbasic01_omali/MainActivity.kt
@@ -2,12 +2,23 @@ package com.omali.kotlinbasic01_omali
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.clickBtn
 import kotlinx.android.synthetic.main.activity_main.smallBtn
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+
+            //코드에 메모 남기기-주석
+            Ctrl+/:커서가 있는 줄을 주석 처리.
+            이{  }안에 있는 코드만, 버튼이 눌렸을때 실행됨.
+
+            Log.d("메인화면로그","클릭용 버튼 눌림")
+        }
 
         smallBtn
     }
