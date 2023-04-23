c61eab6aa25b95e0ed815678520eec53a13d7c36
Omali <misskimlilac68@gmail.com>
2023-04-23 13:19:47 +0900

    버튼2개 추가->기초 이벤트에 사용할 예정

--- app/src/main/java/com/omali/kotlinbasic01_omali/MainActivity.kt
+++ app/src/main/java/com/omali/kotlinbasic01_omali/MainActivity.kt
@@ -2,10 +2,13 @@ package com.omali.kotlinbasic01_omali
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.smallBtn
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        smallBtn
     }
 }
